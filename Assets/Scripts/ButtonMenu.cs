2112572e5d359f918b8d0ce074276a7a50906932
Juntao666 <jl13036@nyu.edu>
2023-04-06 22:43:13 -0400

    comment quit code

--- Assets/Scripts/ButtonMenu.cs
+++ Assets/Scripts/ButtonMenu.cs
@@ -43,11 +43,11 @@ public class ButtonMenu : MonoBehaviour
         SceneManager.LoadScene("start");
     }
 
-    public void QuitGame()
-    {
-    #if !UNITY_WEBGL
-        Application.Quit();
-    #endif
-    }
+    // public void QuitGame()
+    // {
+    // #if !UNITY_WEBGL
+    //     Application.Quit();
+    // #endif
+    // }
 
 }
